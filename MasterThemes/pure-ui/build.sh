9273a020ed10b399b09485f04aec0c25c0eaa6ac
Humpol <81028218+Humpol@users.noreply.github.com>
2021-06-14 17:07:38 +0200

    Update build.sh

--- MasterThemes/pure-ui/build.sh
+++ MasterThemes/pure-ui/build.sh
@@ -21,7 +21,7 @@ if [ ! -f "artisan" ]; then
     zip -r PterodactylBackup-$(date +"%Y-%m-%d").zip public resources 2> /dev/null
 
     echo "Downloading the Theme you picked"
-    mkdir -p tempdown && cd tempdown && git clone https://github.com/Humpol/Pterodactyl-Themes.git .
+    mkdir -p tempdown && cd tempdown && git clone https://github.com/Humpol/pt-themes .
     cp -r MasterThemes/pure-ui/public ..
     cp -r MasterThemes/pure-ui/resources ..
     
